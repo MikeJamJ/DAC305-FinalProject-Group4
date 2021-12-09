d049fc64eb74c82e2fc7c26b783d36a2888c880a
Michael Jamieson <michael.jamielea@gmail.com>
2021-12-09 14:52:12 -0500

    Final commit
    - it is finished

--- DAC305 - Final Project New/Assets/Scripts/SnailGFX.cs
+++ DAC305 - Final Project New/Assets/Scripts/SnailGFX.cs
@@ -11,12 +11,6 @@ public class SnailGFX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
-        // Flip enemy sprite based on direction
-        if (aIPath.desiredVelocity.x >= 0.01f) {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
-        } else if (aIPath.desiredVelocity.x <= -0.01f) {
-            transform.localScale = new Vector3(1f, 1f, 1f);
-        }
-        
+ 
     }
 }
